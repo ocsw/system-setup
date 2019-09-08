03751b19d108944cfa60cfeeaa6a3fdbae3485d9
Danielle Zephyr Malament <danielle.malament@gmail.com>
2019-09-08 03:55:03 -0400

    Fix get-check to work with new file

--- unix-common/git-check.sh
+++ unix-common/git-check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 diff \
-    <(sed 's/^ *//' account-setup.txt | \
+    <(sed 's/^ *//' git.txt | \
         sed 's/ *#.*$//' | sed '/\\$/ {N;s/\\\n//;}' | sed "s@\~/@${HOME}/@" | \
         grep '^git config --global ' | sed 's/^git config --global //' | \
         sed 's/ /=/' | tr -d "'") \
