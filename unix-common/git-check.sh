db72b8cfde0d093f15d482759b23de1108504625
Danielle Zephyr Malament <danielle.malament@gmail.com>
2019-09-08 21:53:25 -0400

    Make git config a script

--- unix-common/git-check.sh
+++ unix-common/git-check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 diff \
-    <(sed 's/^ *//' git.txt | \
+    <(sed 's/^ *//' git-config.sh | \
         sed 's/ *#.*$//' | sed '/\\$/ {N;s/\\\n//;}' | sed "s@\~/@${HOME}/@" | \
         grep '^git config --global ' | sed 's/^git config --global //' | \
         sed 's/ /=/' | tr -d "'") \
